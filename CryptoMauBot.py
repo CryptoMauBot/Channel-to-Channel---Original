5e97ada2c196e0a0103a96e8fa76265ef38108f6
Deenah Mungur <deenah@localhost.localdomain>
2020-05-04 21:22:02 +0400

    asd

--- CryptoMauBot.py
+++ CryptoMauBot.py
@@ -17,8 +17,8 @@ async def my_event_handler(event):
     #destination_user_username='Destination'
     #entity=await client.get_entity("Destination")
     #updates = await client(ImportChatInviteRequest('AAAAAFUe_UyK1izc5Hu86Q'))
-    if str(chat.id)=="1203173236":
-        #await client.send_message(1249866283,event.raw_text)
+    if str(chat.id)=="1299846320":
+        #await client.send_message(,event.raw_text)
         await client.send_message(1306509155, event.message)
 
 client.start()
